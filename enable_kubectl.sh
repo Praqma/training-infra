e3165e96350f70195c00f2223655ce43c45609e4
MichaelVL <mvl.gh@network42.dk>
2018-10-14 10:32:25 +0200

    Nicer enabling of kubectl

--- enable_kubectl.sh
+++ enable_kubectl.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 KEY=testkey
 
-terraform output instance_ips | cut -f 1 -d ',' |xargs -t -IHOST ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $KEY ubuntu@HOST source /tmp/enable_kubectl.sh
+for host in $(terraform output instance_ips | cut -f 1 -d ','); do
+    echo "### Processing host $host"
+    ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $KEY ubuntu@$host source /tmp/enable_kubectl.sh
+done
 
 echo "##########################################"
 echo "## Testing kubectl on all bastion hosts ##"
 echo "##########################################"
-terraform output instance_ips | cut -f 1 -d ',' |xargs -t -IHOST ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $KEY ubuntu@HOST kubectl get componentstatuses
+for host in $(terraform output instance_ips | cut -f 1 -d ','); do
+    echo "### Testing kubectl on host $host"
+    ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $KEY ubuntu@$host kubectl get componentstatuses
+done
