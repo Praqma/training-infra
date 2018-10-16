1955f7021fc264dc87ad9abda7beab2059cad309
MichaelVL <mvl.gh@network42.dk>
2018-10-16 13:49:57 +0200

    Add script to test ssh access and instace readiness

---
+++ test-ssh-access.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+KEY=testkey
+
+echo "######################################"
+echo "## Testing ssh to all bastion hosts ##"
+echo "######################################"
+for host in $(terraform output instance_ips | cut -f 1 -d ','); do
+    echo "### Testing access and uptime of host $host"
+    ssh -q -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $KEY ubuntu@$host uptime
+done
